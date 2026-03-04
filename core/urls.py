4cd4adf9083aff6d278a846c9c26d149d96cadac
Abdulloh-dev-code <abdullohmirzaryev1998@gmail.com>
2026-03-04 08:50:20 +0500

    DJANGO

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import test_view
+from django.urls import path, include
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", test_view)
+    path("", include("main.urls"))
 ]
--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from main.models import Student,Cource
 
-# Register your models here.
+admin.site.register(Student)
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "price", "discount", "discount_type", "final_price"]
+    list_display_links = ["title", "id"]
+    list_filter = ["price"] 
+    search_fields = ["title"]
+
+admin.site.register(Cource, CourseAdmin)
